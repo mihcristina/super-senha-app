dd8c021e21f0e990244f88d894c217485186d096
mihcristina <contato@michellicristina.com.br>
2022-02-11 17:10:34 -0300

    chore: add methods in PasswordsViewController

--- super-senha-app/PasswordsViewController.swift
+++ super-senha-app/PasswordsViewController.swift
@@ -18,10 +18,24 @@ class PasswordsViewController: UIViewController {
     var userCapitalLetters: Bool!
     var userSpecialCharacters: Bool!
     
+    var passwordGenerator: PasswordGenerator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        title = "Total de Senhas: \(numberOfPasswords)"
+        passwordGenerator = PasswordGenerator(numberOfCharacters: numberOfCharacters, useLetters: useLetters, useNumbers: userNumbers, useCapitalLetters: userCapitalLetters, useSpecialCharacters: userSpecialCharacters)
+        
+        generatePasswords()
+    }
+    
+    func generatePasswords(){
+        tvPasswords.scrollRangeToVisible(NSRange(location: 0, length: 0))
+        tvPasswords.text = ""
+        let passwords = passwordGenerator.generate(total: numberOfPasswords)
+        for password in passwords {
+            tvPasswords.text.append(password + "\n\n")
+        }
     }
     
     @IBAction func generate(_ sender: UIButton) {
