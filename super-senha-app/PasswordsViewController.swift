0285b4481746be68f77ca18520ccebef6a3d2edc
mihcristina <contato@michellicristina.com.br>
2022-02-11 03:35:02 -0300

    chore: add outlets in first viewcontroller and add new viewcontroller

---
+++ super-senha-app/PasswordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordsViewController.swift
+//  super-senha-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 11/02/22.
+//
+
+import UIKit
+
+class PasswordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
