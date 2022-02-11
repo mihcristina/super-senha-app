fce4f2764610d5f9ddc39eb44a068a61e8a638ac
mihcristina <contato@michellicristina.com.br>
2022-02-11 03:27:53 -0300

    chore: add main configuration

---
+++ super-senha-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  super-senha-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 11/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
